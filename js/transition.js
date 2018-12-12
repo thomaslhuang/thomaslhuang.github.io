ed2ce5f7090b273790e558236688c5a318d98968
Tommy Huang <tommy@Tommys-MacBook-Pro.local>
2018-12-12 15:25:31 -0500

    Added page transition

---
+++ js/transition.js
@@ -0,0 +1,20 @@
+$("a").on("click", function(event){
+	event.preventDefault()
+
+	const href = $(this).attribute("href")
+
+	window.history.pushState(null, null, href)
+
+	$.ajax({
+		url: href,
+		success: function(data){
+			$("body").fadeOut(500, function(){
+				const newPage = $(data).filter("body").html()
+
+				$("body").html(newPage)
+
+				$("body").fadeIn(500)
+			})
+		}
+	})
+})
