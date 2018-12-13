6ee4ba42b611cc295e77a772b6a208d9a9cb5e7c
Tommy Huang <tommy@Tommys-MacBook-Pro.local>
2018-12-12 22:39:51 -0500

    Testing pages

--- js/transition.js
+++ js/transition.js
@@ -1,20 +1,22 @@
-$("body").not(".external").on("click", "a", function(event){
-	event.preventDefault()
+$("body").on("click", "a", function(event){
+	if (!$(event.target).is(".external")){
+		event.preventDefault()
 
-	const href = $(this).attr("href")
+		const href = $(this).attr("href")
 
-	window.history.pushState(null, null, href)
+		window.history.pushState(null, null, href)
 
-	$.ajax({
-		url: href,
-		success: function(data){
-			$(".container-fluid").fadeOut(500, function(){
-				const newPage = $(data).filter(".container-fluid").html()
+		$.ajax({
+			url: href,
+			success: function(data){
+				$(".container-fluid").fadeOut(500, function(){
+					const newPage = $(data).filter(".container-fluid").html()
 
-				$(".container-fluid").html(newPage)
+					$(".container-fluid").html(newPage)
 
-				$(".container-fluid").fadeIn(500)
-			})
-		}
-	})
+					$(".container-fluid").fadeIn(500)
+				})
+			}
+		})
+	}
 })
