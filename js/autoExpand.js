fb16543131015403e89ae0cfb97e4270ef378024
Tommy Huang <tommy@Tommys-MacBook-Pro.local>
2019-01-03 17:13:19 -0800

    added autoexpand

--- js/autoExpand.js
+++ js/autoExpand.js
@@ -1,14 +1,21 @@
-// Applied globally on all textareas with the "autoExpand" class
-$(document)
-    .one('focus.autoExpand', 'textarea.autoExpand', function(){
-        var savedValue = this.value;
-        this.value = '';
-        this.baseScrollHeight = this.scrollHeight;
-        this.value = savedValue;
-    })
-    .on('input.autoExpand', 'textarea.autoExpand', function(){
-        var minRows = this.getAttribute('data-min-rows')|0, rows;
-        this.rows = minRows;
-        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
-        this.rows = minRows + rows;
-    });
+var comfyText = (function(){
+  var tag = document.querySelectorAll('textarea')
+  for (var i=0; i<tag.length; i++){
+    tag[i].addEventListener('paste',autoExpand)
+    tag[i].addEventListener('input',autoExpand)
+    tag[i].addEventListener('keyup',autoExpand)
+  }
+  function autoExpand(e,el){
+    var el = el || e.target
+    el.style.height = 'inherit'
+    el.style.height = el.scrollHeight+'px'
+  }
+  window.addEventListener('load',expandAll)
+  window.addEventListener('resize',expandAll)
+  function expandAll(){
+    var tag = document.querySelectorAll('textarea')
+    for (var i=0; i<tag.length; i++){
+      autoExpand(e,tag[i])
+    }
+  }
+})()
